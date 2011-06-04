d8d2ed5d140ccc102674bf41067e5930635d67f7
Ed Phillips <ed@cronos.net>
2011-06-03 22:42:46 -0700

    delayed_job mods for both app and admin

--- cookbooks/delayed_job/recipes/default.rb
+++ cookbooks/delayed_job/recipes/default.rb
@@ -5,11 +5,19 @@
 
 if node[:name] !~ /^util.*?/
   node[:applications].each do |app_name,data|
-    next if app_name == 'admin'
-    
     # determine the number of workers to run based on instance size
+    if node[:instance_role] == 'solo'
+      worker_count = 1
+    else
+      case node[:ec2][:instance_type]
+      when 'm1.small': worker_count = 1
+      when 'c1.medium': worker_count = 3
+      when 'c1.xlarge': worker_count = 8
+      else
+        worker_count = 1
+      end
+    end
     
-    worker_count = 2
     
     
     worker_count.times do |count|
@@ -19,18 +27,25 @@ if node[:name] !~ /^util.*?/
         group "root"
         mode 0644
         variables({
-          :app_name => app_name,
-          :user => node[:owner_name],
-          :worker_name => "#{app_name}_delayed_job#{count+1}",
-          :framework_env => node[:environment][:framework_env]
-        })
+                    :app_name => app_name,
+                    :user => node[:owner_name],
+                    :worker_name => "#{app_name}_delayed_job#{count+1}",
+                    :framework_env => node[:environment][:framework_env]
+                  })
       end
     end
     
-    execute "monit-reload-restart" do
-       command "sleep 30 && monit reload && monit restart all dj_#{app_name}"
-       action :run
+    
+    ey_cloud_report "delayed_job" do
+      message "about to restart (#{worker_count}) workers"
+      message "workers are for node #{node[:name]} and for #{app_name} "
     end
-      
+
+
+    execute "monit-reload-restart" do
+      command "sleep 30 && monit reload && monit restart all dj_#{app_name}"
+      action :run
+    end
+    
   end
 end
